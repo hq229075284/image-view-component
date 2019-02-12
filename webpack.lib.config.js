50915f1ff351ae0bc6f6c7723556190f3a852713
HanQ <229075284@qq.com>
2019-02-12 13:49:40 +0800

    complete lib config

--- webpack.lib.config.js
+++ webpack.lib.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const dev = require('./webpack.dev.config.js')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(dev, {
   mode: 'production',
@@ -8,10 +9,34 @@ module.exports = merge(dev, {
     dragScale: path.resolve(__dirname, './src/components/index.ts')
   },
   output: {
-    libraryTarget: "commonjs2"
-  },
-  plugins: [],
-  devServer: {}
+    library: '[name]',
+    libraryTarget: "umd",
+    libraryExport: "Adapter"
+  }
 })
+
+const packCss = {
+  test: /\.less$/,
+  use: [
+    MiniCssExtractPlugin.loader,
+    "css-loader",
+    "less-loader"
+  ]
+}
+
+module.exports.module.rules.splice(module.exports.module.rules.length - 1, 1, packCss)
+
 delete module.exports.plugins
 delete module.exports.devServer
+delete module.exports.devtool
+
+module.exports.plugins = [
+  new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // both options are optional
+    filename: "[name].css",
+    chunkFilename: "[id].css"
+  })
+]
+
+// console.log(JSON.stringify(module.exports, null, 2))
